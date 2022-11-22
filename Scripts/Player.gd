daf773b3999f7ba38e8882ef4563a631077ffdc9
Antoine Ouellette <2143781@carrefour.cegepvicto.ca>
2022-11-22 18:18:36 -0500

    code basic pour les mouvements pour pouvoir faire des test

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,2 +1,22 @@
 extends KinematicBody2D
 
+var speed = 600
+var jump_speed = 1500
+var gravity = 4500
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	velocity.x = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	velocity.y += gravity*delta
+	
+	move_and_slide(velocity)
+
+func _input(event):
+	if event.is_action_pressed("ui_up"):
+		velocity.y = -jump_speed
