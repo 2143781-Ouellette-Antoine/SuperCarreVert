625689dd12454e27513a333bf0b719ffc53cf536
Gmdel <2166311@etudiant.cegepvicto.ca>
2022-12-06 13:38:54 -0500

    pre pre-remise

--- Scripts/EnnemyBarnacle.gd
+++ Scripts/EnnemyBarnacle.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
 func _on_Barnacle_area_entered(area: Node):
-
+	
 	emit_signal("hit")
-
-	queue_free()
+	#queue_free()
+	$Sprite.hide()
+	$BarnaDead.show()
+	$CollisionShape2D.disabled = true
+	hide()
+	yield(get_tree().create_timer(0.2), "timeout")
+	show()
+	$CollisionShape2D.disabled = true
+	yield(get_tree().create_timer(0.2), "timeout")
+	hide()
+	yield(get_tree().create_timer(0.2), "timeout")
+	show()
+	$CollisionShape2D.disabled = true
+	
